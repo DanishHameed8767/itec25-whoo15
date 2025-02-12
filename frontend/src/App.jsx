536ef6cc0a15b9a483e7381463e0863c254dc8ab
Danish Hameed <danishhameed993@gmail.com>
2025-02-12 19:41:23 +0500

    minor fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from './Home';
+import Login from './components/login';
+import SignUp from './components/SignUp';
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<SignUp />} />
     </Routes>
   </BrowserRouter>
     </>
